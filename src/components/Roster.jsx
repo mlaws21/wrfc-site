980f178079304809534cc0b73323c94952a19d97
William Howe <wsh2@williams.edu>
2023-01-07 16:51:06 -0500

    roster update

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -1,7 +1,13 @@
+import roster from './files/roster.jsx';
+
 function Roster() {
     return (
         <dev>
             <h1>Roster</h1>
+            <div id='roster'>
+            <p id='rosterHead'>Players</p>
+                <p id='newsBody'>{roster}</p>
+            </div>
         </dev>
     )
 }
---
+++ src/components/files/roster.jsx
@@ -0,0 +1,3 @@
+const roster= "William Howe: Flanker \n Matt Laws: Hooker \n Will Gallon: THE BIG GUY \n";
+
+export default roster;
