9507263f4c4e17aee6d3567882dab1438cc7135c
Matt Laws <mdl4@williams.edu>
2023-01-07 16:32:05 -0500

    directions & merch

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -1,10 +1,11 @@
+import logo from "./images/logo.png";
+import "./style/Merch.css"
 function Merch() {
     return (
-        <div>
-            <iframe id="iFrameExample"
-                title="iFrame Example"
-                src="https://tytanrugby.com/collections/williams-college-rugby-football-club">
-            </iframe>
+        <div id="center">
+            <p id="title">Buy Merch Here</p>
+            <img src={logo} alt="Logo" id="logo"/>
+            <a id="link"href="https://tytanrugby.com/collections/williams-college-rugby-football-club">Click this Link</a>
         </div>
     )
 }
