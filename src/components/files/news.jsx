0cd14516d9dab35fb0c280059083bb6cdc99195e
William Howe <wsh2@williams.edu>
2023-01-18 13:58:02 -0500

    added news

--- src/components/files/news.jsx
+++ src/components/files/news.jsx
@@ -1,3 +1,3 @@
-const text= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+const text= "The White Dogs beat Amherst 28-27 in their final game of the season. This victory marks the WRFC's third consecutive victory against 'Herst";
 
 export default text;
