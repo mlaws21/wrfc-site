abb5c3de541ec1fdac5b79ab34ae639035147b07
William Howe <wsh2@williams.edu>
2023-01-17 22:19:33 -0500

    added About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,13 @@ function About() {
     return (
         <dev>
             <h1>About</h1>
+            <p id="note">
+            The Williams Rugby Football Club (WRFC) was founded by David Coughlin '61 in the spring of 1959 following spending a 
+            year studying abroad at Oxford University in England. Starting with a handful of incoming freshmen with experience 
+            playing rugby while studying in England, the Club was formed and supported by Peter Pearson, the first Williams Rugby coach. 
+            The following fall, the WRFC entered the Eastern Rugby Union (ERU) and won the ERU Championship in 1960 with a 5-0-1 record. 
+            Following a 1961 tour in England, support and recognition from the College increased dramatically.
+            </p>
         </dev>
     )
 }
