0fe2c30d476bb27cd396591e9e5ab7450b4d2379
Matt Laws <mdl4@williams.edu>
2023-01-18 13:43:04 -0500

    updated css pages

---
+++ src/components/files/scheduleHelp.jsx
@@ -0,0 +1,25 @@
+const schedule = [
+    
+    //["Date", "Team", "Location", "Time", "Final Score"],
+    ["Spring 2023", 
+        [
+            ["Date", "Team", "Location", "Time", "Final Score"],
+            ["April 8, 2023", "Union Tournament", "Union College", "TBD", "TBD"],
+            ["April 15, 2023", "JD Tournament", "Home", "TBD", "TBD"],
+            ["April 29, 2023", "Amherst", "Home", "TBD", "TBD"]
+        ]
+    ],
+
+    ["Fall 2022", 
+    [
+        ["Date", "Team", "Location", "Time", "Final Score"],
+        ["September 17, 2022", "Trinity", "Home", "12:00pm", "14-28 (Loss)"],
+        ["October 1, 2022", "Union", "Union", "11:00am", "21-14 (Win)"],
+        ["October 15, 2022", "ECSU", "Home", "12:00pm", "28-35 (Loss)"],
+        ["October 29, 2022", "Amherst", "Amherst", "1:00pm", "28-27 (Win)"]            
+    ]
+]
+
+]
+
+export default schedule;
